70616fb46de049c3c3fe062a998acbffe3912eec
Bcansdale <28786879+Bcansdale@users.noreply.github.com>
2024-08-02 13:22:05 -0600

    2 questions left

--- src/arrays.js
+++ src/arrays.js
@@ -66,7 +66,18 @@ console.log(itemsWithLength(['a', 'bbb', 'cccc', 'dddddd', 'eee'], 3));
 // Ex.:
 //   everyOtherItem(['a', 'b', 'c', 'd', 'e']);
 //   => ['a', 'c', 'e']
-function everyOtherItem(items) {}
+function everyOtherItem(items) {
+  let result = [];
+
+  for (let i = 0; i <= items.length; i++) {
+      if (i % 2 === 0) {
+          result.push(items[i]);
+      }
+  }
+  return result;
+}
+
+console.log(everyOtherItem(['a', 'b', 'c', 'd', 'e']));
 
 // Given a list of words and a letter, return the indexes of the words that
 // start with that letter. You can assume that the words and letter will always
@@ -74,7 +85,18 @@ function everyOtherItem(items) {}
 // Ex.:
 //   findWordsStartingWith(['apple', 'banana', 'kiwi', 'pear', 'bacon'], 'b');
 //   => [1, 4]
-function findWordsStartingWith(words, letter) {}
+function findWordsStartingWith(words, letter) {
+  let result = [];
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].startsWith(letter)) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+console.log(findWordsStartingWith(["apple", "banana", "kiwi", "pear", "bacon"], "b"));
 
 // Return the `n` smallest values in the array in descending order (largest
 // numbers first). Assume that `n` will always be less than the length of the
@@ -82,7 +104,15 @@ function findWordsStartingWith(words, letter) {}
 // Ex.:
 //   smallestNItems([1, 30, 4, 21, 100000], 3);
 //   => [21, 4, 1]
-function smallestNItems(items, n) {}
+function smallestNItems(items, n) {
+  let result = items.sort(function(a, b){
+    return a - b;
+  }).slice(0, n);
+
+  return result.reverse();
+}
+
+console.log(smallestNItems([1, 30, 4, 21, 100000], 3));
 
 // Search for a value in the array and return its index. If the value appears
 // more than once, return the index of the *FIRST* occurrence of the value. If
